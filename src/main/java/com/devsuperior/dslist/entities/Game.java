61a6660974cf37deded2c90227d782bbeb69946c
Luiz Mendes <luizmendes0897@gmail.com>
2025-01-25 13:38:30 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -18,20 +18,24 @@ public class Game {
 	private Integer year;
 	private String genre;
 	private String platforms;
+	private double score;
 	private String imgUrl;
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	 public Game() {
 	    }
 	    
-		public Game(Long id, String title, Integer year, String genre, String platforms, String imgUrl,
+		public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 				String shortDescription, String longDescription) {
 			this.id = id;
 			this.title = title;
 			this.year = year;
 			this.genre = genre;
 			this.platforms = platforms;
+			this.score = score;
 			this.imgUrl = imgUrl;
 			this.shortDescription = shortDescription;
 			this.longDescription = longDescription;		
@@ -75,6 +79,14 @@ public class Game {
 
 		public void setPlatforms(String platforms) {
 			this.platforms = platforms;
+		}
+		
+		public double getScore() {
+			return score;
+		}
+
+		public void setScore(double score) {
+			this.score = score;
 		}
 
 		public String getImgUrl() {
